2f9302dc0a1175ea8cbdd3eef5c1485d7ed6b6e8
Vega Studio <Fidgeter@mail.ru>
2017-09-24 20:01:04 +0300

    Update dashboard.php

--- admin/View/dashboard.php
+++ admin/View/dashboard.php
@@ -4,9 +4,9 @@
     <div class="container">
         <br><br><br>
         <div style="text-align:center;">
-            <h1><i class="icon-heart icons"></i> Welcome to CMS</h1>
+            <h1><i class="empty heart icon"></i> Welcome to <img class="ui middle aligned small image" src="/admin/Assets/images/logo.png"> cms</h1>
             Watch on channel  <a href="https://www.youtube.com/channel/UCOy440HnmqURkjG9CrBi6nw?disable_polymer=true">
-                <i class="icon-social-youtube icons"></i> Developer Start
+                <i class="youtube play icon"></i> Developer Start
             </a>
         </div>
     </div>
