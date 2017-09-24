a6a9a9ede84fa00e65306c5e2c7938d1883d7856
Vega Studio <Fidgeter@mail.ru>
2017-09-24 18:04:07 +0300

    Update menu_item.php

--- admin/View/setting/menu_item.php
+++ admin/View/setting/menu_item.php
@@ -1,12 +1,12 @@
 <li class="menu-item-<?= $item->id ?>" data-id="<?= $item->id ?>">
-    <i class="icon-pencil icons"></i>
+    <i class="write icon"></i>
     <input type="text" value="<?= $item->name ?>" onchange="menu.updateItem(<?= $item->id ?>, 'name', this)">
-    <i class="icon-link icons"></i>
+    <i class="linkify icon"></i>
     <input type="text" value="<?= $item->link ?>" onchange="menu.updateItem(<?= $item->id ?>, 'link', this)">
     <div class="menu-item-event">
         <button class="button-remove" onclick="menu.removeItem(<?= $item->id ?>)">
-            <i class="icon-trash icons"></i>
+            <i class="remove circle icon"></i>
         </button>
-        <i class="icon-cursor-move icons"></i>
+        <i class="move icon"></i>
     </div>
 </li>
