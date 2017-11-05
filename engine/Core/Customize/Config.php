22ddf3aa493d950a2321750dbdfc9760c32552c1
Vega Studio <Fidgeter@mail.ru>
2017-11-05 16:49:46 +0300

    Add Semaintic UI icons

--- engine/Core/Customize/Config.php
+++ engine/Core/Customize/Config.php
@@ -13,27 +13,27 @@ class Config
     protected $config = [
         'dashboardMenu' => [
             'home' => [
-                'classIcon' => 'icon-speedometer icons',
+                'classIcon' => 'dashboard icon',
                 'urlPath'   => '/admin/',
                 'title'     => 'Home'
             ],
             'pages' => [
-                'classIcon' => 'icon-doc icons',
+                'classIcon' => 'file icon',
                 'urlPath'   => '/admin/pages/',
                 'title'     => 'Pages'
             ],
             'posts' => [
-                'classIcon' => 'icon-pencil icons',
+                'classIcon' => 'write icon',
                 'urlPath'   => '/admin/posts/',
                 'title'     => 'Posts'
             ],
             'plugins' => [
-                'classIcon' => 'icon-wrench icons',
+                'classIcon' => 'plug icon',
                 'urlPath'   => '/admin/plugins/',
                 'title'     => 'Plugins'
             ],
             'settings' => [
-                'classIcon' => 'icon-equalizer icons',
+                'classIcon' => 'setting icon',
                 'urlPath'   => '/admin/settings/general/',
                 'title'     => 'Settings'
             ]
